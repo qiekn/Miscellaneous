a614c0e57354643eabf64b619c3b1399acdba014
qiekn <qiekn@kimidayo.cn>
2024-12-24 00:32:47 +0800

    feat: add editorconfig & format

--- Rotate.cs
+++ Rotate.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
 
-namespace ck.qiekn.Miscellanies
-{
-    public class Rotate : MonoBehaviour
-    {
+namespace ck.qiekn.Miscellanies {
+    public class Rotate : MonoBehaviour {
         [SerializeField] float speed = 15f;
         [SerializeField] Vector3 multiplier = new Vector3(1f, 2f, 3f);
 
-        private void Update()
-        {
+        private void Update() {
             transform.Rotate(multiplier * speed * Time.deltaTime);
         }
     }
